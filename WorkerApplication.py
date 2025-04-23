978d55b0a08f880f8d921bd2396ac0c73453846c
TalkinWhip <vmolaliyski@tutanota.com>
2025-04-23 13:34:05 +0200

    small formatting changes and convenience features

--- WorkerApplication.py
+++ WorkerApplication.py
@@ -5,16 +5,24 @@ from pyzeebe import ZeebeClient, ZeebeWorker, ZeebeTaskRouter, create_camunda_cl
 # Setup logging
 # logging.basicConfig(level=logging.Debug)
 
+def get_customer_credit(customer_id: str):
+    return customer_id[-2:]
+
+def deduct_credit(customer_id: str, amount: float):
+    open_amount = amount - float(get_customer_credit(customer_id))
+    return open_amount
+
 # Define tasks
 router = ZeebeTaskRouter()
 
 @router.task("example-job")
-def deduct_credit(job: Job):
+def handle_example_task(job: Job):
     print(f"Hello world")
     return
 
 # Create a channel, the worker and include the router with tasks
 async def main():
+    print("Worker starting")
     grpc_channel = create_camunda_cloud_channel(client_id="xxx",
                                                 client_secret="xxx",
                                                 cluster_id="xxx",
